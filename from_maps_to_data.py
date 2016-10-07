d2e4ec483d527254d39c3ceb3f9d131f92d61df9
Lumir Balhar <lbalhar@redhat.com>
2016-10-07 23:49:09 +0200

    Added final data

---
+++ from_maps_to_data.py
@@ -0,0 +1,24 @@
+import json
+import sys
+from glob import glob
+
+if len(sys.argv) < 2:
+    print('Generating data for all maps')
+    maps = glob('maps/*.txt')
+else:
+    maps = [sys.argv[1]]
+
+data = {}
+
+for map in maps:
+    print('Processing map {}'.format(map))
+    name = map.split('/')[1].split('.')[0].lstrip('flag-of-')
+    with open(map, 'r') as mapfile:
+        content = json.load(mapfile)
+        flagdata = []
+        for line in content['data']:
+            for point in line:
+                flagdata.append(tuple(content['palette'][point]))
+        data[name] = flagdata
+
+print(data)
